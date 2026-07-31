1a81f4492d31a53fa073af1649648d2fbc495910
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-07-31 10:50:37 +0530

    Day1

---
+++ day1/index.js
@@ -0,0 +1,23 @@
+const app = require('./app.js') 
+// var a =20
+// var b = 20
+// var c = 20
+// console.log(a+b+c)
+
+// var x =20
+// if(x==30){
+//     console.log("x is 20")
+// }
+
+// for(var i =0; i<5; i++){
+//     console.log(i)
+// }
+
+// var x =20x
+// const arr = [1,3,5,2,4]
+// for(let i =0; i<arr.length; i++){
+//     console.log(arr[i])
+//     console.log(arr[2])
+// }
+
+console.log(app)
