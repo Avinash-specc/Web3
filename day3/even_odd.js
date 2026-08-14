5d55b407f845bcdc9765cf9fdfc424dd6fe06911
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-08-14 11:16:45 +0530

    Day3 -I

---
+++ day3/even_odd.js
@@ -0,0 +1,12 @@
+
+
+function checkNumber(number){
+    if(number%2 ==0){
+        console.log("It is an even number")
+    }
+    else{
+        console.log("It is an odd number")
+    }
+}
+
+checkNumber(49);
---
+++ day3/percentage.js
@@ -0,0 +1,8 @@
+function percentage(maths,english,science){
+    return (maths+english+science)/3;
+}
+
+maths = 100
+english = 89
+science = 99
+console.log("Percentage: ",percentage(maths,english,science),"%")
