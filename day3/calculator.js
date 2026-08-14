774dd922f595bfad8fbc045862db7dc9da35d74a
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-08-14 11:15:21 +0530

    Calculator in js

---
+++ day3/calculator.js
@@ -0,0 +1,20 @@
+a =49
+b = 34
+
+function add(a,b){
+    console.log("Addition: ",a+b)
+}
+function sub(a,b){
+    console.log("Subtraction: ",a-b)
+}
+function mul(a,b){
+    console.log("Multiplication: ",a*b)
+}
+function divide(a,b){
+    console.log("Division: ",a/b)
+}
+
+add(a,b)
+sub(a,b)
+mul(a,b)
+divide(a,b)
