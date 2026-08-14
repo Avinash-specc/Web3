edf1e4a1f657546a956b63ecdba2b34f18de67f1
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-08-14 11:16:27 +0530

    Student Information

---
+++ day3/studentInformation.js
@@ -0,0 +1,15 @@
+console.log("\n\n\n================ Student Information ==================\n\n\n")
+let name = "Avinash"
+let id = 17
+let branch = "AIML"
+let department = "CSE"
+
+
+console.log(name)
+console.log(id)
+console.log(branch)
+console.log(department)
+console.log("\n\n")
+
+
+
