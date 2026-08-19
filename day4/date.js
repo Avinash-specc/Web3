a93c1136749bf3e04219040b22de6b661e508d1a
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-08-19 14:20:28 +0530

    Date and Math in js

--- day4/date.js
+++ day4/date.js
@@ -3,3 +3,13 @@ console.log(date)
 console.log("Year: ",date.getFullYear())
 console.log("Month: ",date.getMonth())
 console.log("Date: ",date.getDate())
+
+let arr = [23,5,6,34,43]
+let num = 9.4
+console.log("PI:",Math.PI)
+console.log("Minimum:",Math.min(...arr))
+console.log("Round:",Math.round(num))
+console.log("Floor:",Math.floor(num))
+console.log("ceil:",Math.ceil(num))
+console.log("Power:",Math.pow(2,5))
+console.log("Sqrt:",Math.sqrt(16))
